e9fc4ad94c81feda65fb996a8a6909ddbf71ee4f
Resha Puspita <reshapuspita734@gmail.com>
2020-03-05 09:39:29 +0700

    Add files via upload

---
+++ sketch_mar05a.ino
@@ -0,0 +1,19 @@
+#include <Ultrasonic.h>
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+Ultrasonic ultrasonic(2,5);
+BluetoothSerial, SerialBT;
+int distance;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(23,OUTPUT);
+  SerialBT.begin("UTSEmbeddedSystem"); // device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  Serial.println("Please Wait...");
+  delay(7000);
+}
